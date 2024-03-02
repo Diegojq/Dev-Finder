6f059c9ca9784b880da2a740fb4193a057677b9a
diegojq <dajqprogramming@gmail.com>
2024-03-02 16:25:26 -0500

    :sparkles: Reset ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const buttonDark = document.getElementById("btn-dark");
 const buttonSearch = document.getElementById("btn-search");
 const inputSearch = document.getElementById("input-search");
+const spanResults = document.getElementById("span-results");
 
-var profileName = document.getElementById("profile-name");
-var profileUser = document.getElementById("profile-username");
-var profileJoined = document.getElementById("profile-joined");
+const profileImage = document.getElementById("profile-image");
+const profileName = document.getElementById("profile-name");
+const profileUser = document.getElementById("profile-username");
+const profileJoined = document.getElementById("profile-joined");
+const profileDescription = document.getElementById("profile-description");
+const publicRepos = document.getElementById("public-repos");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const userLocation = document.getElementById("user-location");
+const userBlog = document.getElementById("user-blog");
+const userTiwtter = document.getElementById("user-tiwtter");
+const userCompany = document.getElementById("user-company");
 
 buttonDark.addEventListener("click", toggleDarkMode);
 buttonSearch.addEventListener("click", searchUser);
@@ -22,6 +32,7 @@ async function searchUser() {
     resetUi(data);
     console.log(data);
   } catch (error) {
+    spanResults.innerText = "No results";
     console.error(error);
   }
 }
@@ -49,11 +60,38 @@ function formatJoinedDate(dateString) {
 }
 
 function resetUi(data) {
+  const image = data.avatar_url;
   const name = data.name;
   const user = data.login;
   const joined = data.created_at;
 
+  const bio = data.bio || "";
+
+  const repos = data.public_repos;
+  const myFollowers = data.followers;
+  const myfollowing = data.following;
+
+  const location = data.location || "Not Avilable";
+  const blog = data.blog || "#";
+  const tiwtter = `@${data.tiwtter_username} ` || "Not Avalible";
+  const company = data.company || "Not Avalible";
+
+  profileImage.src = image;
   profileName.textContent = name;
   profileUser.textContent = `@${user}`;
   profileJoined.textContent = formatJoinedDate(joined);
+  profileDescription.textContent = bio;
+  publicRepos.textContent = repos;
+  followers.textContent = myFollowers;
+  following.textContent = myfollowing;
+  userLocation.textContent = location;
+  userBlog.href = blog;
+  userTiwtter.textContent = tiwtter;
+  userCompany.textContent = company;
 }
+
+inputSearch.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    resetUi();
+  }
+});
